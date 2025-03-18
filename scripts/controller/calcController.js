523cbd5343776295f421e80e65629bc4b5401764
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-03-18 11:59:15 -0300

    feat: add for loop

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -70,12 +70,28 @@ constructor(){
         calc(){
 
             let last = this._operation.pop();
+
             let result = eval(this._operation.join(" "));
+
             this._operation = [result, last];
+            
+            this.setLastNumberToDisplay();
           
         }
 
         setLastNumberToDisplay(){
+
+            let lastNumber;
+
+            for(let i = this._operation.length-1; i >= 0; i--){
+
+                if (!this.isOperator(this._operation[i])) {
+                    lastNumber = this._operation[i];
+                    break;
+                }
+            }
+
+              this.displayCalc = lastNumber;
 
         }
 
@@ -94,6 +110,8 @@ constructor(){
         } else {
 
             this.pushOperation(value);
+
+            this.setLastNumberToDisplay();
         }
 
         } else { 
