ae0a31600232019a746c3fa063f5938d4166916f
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-03-19 09:14:54 -0300

    feat: implement keyboard key

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -13,7 +13,8 @@ constructor(){
     this._currentDate;
     this.initialize();
     this.initButtonsEvents();
-}
+    this.initkeyboard();
+}   
 
     initialize(){
 
@@ -27,6 +28,56 @@ constructor(){
 
         this.setLastNumberToDisplay();
           
+    }
+
+    initkeyboard(){
+
+        document.addEventListener(keyup, e =>{
+
+            switch (e.key){
+                case 'Escape':
+                    this.clearAll();
+                break;
+    
+                case 'Backspace':
+                    this.clearEntry();
+                break;
+    
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '%':
+                    this.addOperation('e.key');
+                break;
+
+                case 'Enter':
+                case '=':
+                    this.calc();
+                break;
+    
+                case '.':
+                case ',':
+                    this.addDot();
+                break;
+                
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    this.addOperation(parseInt(e.key));
+                    break;
+
+            }
+    
+        });
+
     }
 
     addEventListenerAll(element, events, fn){
